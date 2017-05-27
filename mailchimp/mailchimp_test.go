0fe4041ad42a3b4c2c8b8d6ced59c9dc87c044a6
Ciro S. Costa <ciro.costa@liferay.com>
2017-05-27 13:32:17 -0300

    Adds real test for mailchimp

--- mailchimp/mailchimp_test.go
+++ mailchimp/mailchimp_test.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"testing"
 
 	"github.com/beldpro-ci/subscriber/mailchimp"
@@ -13,30 +12,34 @@ import (
 
 var ts *httptest.Server
 
-func memberMockHandler(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(r)
-}
+func TestSubscribe(t *testing.T) {
+	ts = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user, pass, ok := r.BasicAuth()
+		assert.True(t, ok)
+		assert.Equal(t, "anyuser", user)
+		assert.Equal(t, "key", pass)
 
-func TestMain(m *testing.M) {
-	ts = httptest.NewServer(http.HandlerFunc(memberMockHandler))
+		contentTypeHeader := r.Header.Get("Content-Type")
+		assert.NotEmpty(t, contentTypeHeader)
+		assert.Equal(t, "application/json", contentTypeHeader)
+
+		decoder := json.NewDecoder(r.Body)
+		defer r.Body.Close()
+		var member = new(mailchimp.Member)
+		err := decoder.Decode(member)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "email", member.EmailAddress)
+		assert.Equal(t, "subscribed", member.Status)
+	}))
 	defer ts.Close()
-	os.Exit(m.Run())
-}
 
-func TestSubscribeUser_requestIsAuthenticated(t *testing.T) {
 	mc, err := mailchimp.New(mailchimp.Config{
 		ListId: "123",
 		APIKey: "key",
 		URL:    ts.URL,
 	})
 	assert.NoError(t, err)
-
 	err = mc.Subscribe("email")
 	assert.NoError(t, err)
-}
-
-func TestSubscribeUser_containsData(t *testing.T) {
-}
-
-func TestSubscribeUser_containsContentTypeHeader(t *testing.T) {
 }
